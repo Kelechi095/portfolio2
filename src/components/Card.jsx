082ab0deadbc2284343b7644a5f1c3c711a6aa28
Awoms Kelechi <awomskelechi@gmail.com>
2024-03-12 10:12:53 +0100

    modified styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,10 @@ import CardButtons from "./buttons/CardButtons";
 /* eslint-disable react/prop-types */
 const Card = () => {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
-    <div className="w-full mt-4 bg-card3 p-4 rounded mb-4 border text-xl font-semibold border-zinc-700 shadow-sm text-text_transparent ">
+    <section className="">
+      <div className="w-full mt-4 bg-card3 p-4 rounded mb-4 border text-xl font-semibold border-zinc-700 shadow-sm text-text_transparent ">
         <div>
-          <div className="flex items-center mx-auto">
-            <div className="w-[40%] h-[1px] bg-gradient-to-r from-text_green mx-2"></div>
-            <h2 className="text-lg text-text_color">Technologies</h2>
-            <div className="w-[40%] h-[1px] bg-gradient-to-l from-text_green mx-2"></div>
-          </div>
-          <div className="grid grid-cols-3 gap-2 mt-2">
+          <div className="grid grid-cols-4 gap-2 mt-2">
             <CardButtons title="JavaScript" />
             <CardButtons title="TypeScript" />
             <CardButtons title="Node.js" />
@@ -20,19 +15,7 @@ const Card = () => {
             <CardButtons title="Express.js" />
             <CardButtons title="MongoDB" />
             <CardButtons title="PostgreSQL" />
-          </div>
-        </div>
-        
-    </div>
-    <div className="w-full mt-4 bg-card3  p-4 rounded mb-4 border text-xl font-semibold border-zinc-700 shadow-sm text-text_transparent ">
-        <div>
-          <div className="flex items-center mx-auto">
-            <div className="w-[40%] h-[1px] bg-gradient-to-r from-text_green mx-2"></div>
-            <h2 className="text-lg text-text_color">Tools</h2>
-            <div className="w-[40%] h-[1px] bg-gradient-to-l from-text_green mx-2"></div>
-          </div>
-          <div className="grid grid-cols-3 gap-2 mt-2">
-          <CardButtons title="ReactQuery" />
+            <CardButtons title="ReactQuery" />
             <CardButtons title="Redux" />
             <CardButtons title="Prisma" />
             <CardButtons title="Tailwind" />
@@ -42,12 +25,9 @@ const Card = () => {
             <CardButtons title="Github" />
           </div>
         </div>
-        
-    </div>
+      </div>
     </section>
   );
 };
 
 export default Card;
-
-
