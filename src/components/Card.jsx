f1e0012a9e278e0277f10ea0e14d49f6cba13a04
Awoms Kelechi <awomskelechi@gmail.com>
2024-03-12 21:07:24 +0100

    added icons to card buttons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,20 @@
 import CardButtons from "./buttons/CardButtons";
+import { FaHtml5 } from "react-icons/fa";
+import { IoLogoCss3 } from "react-icons/io";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiTypescript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaNodeJs } from "react-icons/fa6";
+import { SiExpress } from "react-icons/si";
+import { SiPostgresql } from "react-icons/si";
+import { DiMongodb } from "react-icons/di";
+import { SiPrisma } from "react-icons/si";
+import { SiReactquery } from "react-icons/si";
+import { SiRedux } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
+import { FaGitAlt } from "react-icons/fa";
+import { FaNpm } from "react-icons/fa";
 
 /* eslint-disable react/prop-types */
 const Card = () => {
@@ -7,22 +23,22 @@ const Card = () => {
       <div className="w-full mt-4 bg-card3 p-4 rounded mb-4 border text-xl font-semibold border-zinc-700 shadow-sm text-text_transparent ">
         <div>
           <div className="grid grid-cols-3 md:grid-cols-4 gap-2 mt-2">
-            <CardButtons title="JavaScript" />
-            <CardButtons title="TypeScript" />
-            <CardButtons title="Node.js" />
-            <CardButtons title="React.js" />
-            <CardButtons title="Next.js" />
-            <CardButtons title="Express.js" />
-            <CardButtons title="MongoDB" />
-            <CardButtons title="PostgreSQL" />
-            <CardButtons title="ReactQuery" />
-            <CardButtons title="Redux" />
-            <CardButtons title="Prisma" />
-            <CardButtons title="Tailwind" />
-            <CardButtons title="Axios" />
-            <CardButtons title="Git" />
-            <CardButtons title="Npm" />
-            <CardButtons title="Github" />
+            <CardButtons title="HTML5" icon={FaHtml5}/>
+            <CardButtons title="CSS3" icon={IoLogoCss3}/>
+            <CardButtons title="JavaScript" icon={IoLogoJavascript}/>
+            <CardButtons title="TypeScript" icon={SiTypescript} />
+            <CardButtons title="Node.js" icon={FaNodeJs}/>
+            <CardButtons title="React.js" icon={FaReact}/>
+            <CardButtons title="Next.js" icon={TbBrandNextjs}/>
+            <CardButtons title="Express.js" icon={SiExpress}/>
+            <CardButtons title="MongoDB" icon={DiMongodb}/>
+            <CardButtons title="PostgreSQL" icon={SiPostgresql}/>
+            <CardButtons title="ReactQuery" icon={SiReactquery}/>
+            <CardButtons title="Redux" icon={SiRedux}/>
+            <CardButtons title="Prisma" icon={SiPrisma}/>
+            <CardButtons title="Tailwind" icon={SiTailwindcss}/>
+            <CardButtons title="Git" icon={FaGitAlt}/>
+            <CardButtons title="NPM" icon={FaNpm} hidden/>
           </div>
         </div>
       </div>
--- src/components/buttons/CardButtons.jsx
+++ src/components/buttons/CardButtons.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/prop-types */
 
-const CardButtons = ({ title }) => {
+const CardButtons = ({ title, hidden, icon: Icon }) => {
   return (
-    <div className="border border-text_transparent py-2 px-4 text-text_color font-semibold  hover:border-text_color md:file:transition duration-300 text-xs md:text-base flex justify-center">
+    <div
+      className={`
+    border border-text_transparent py-2 px-4 text-text_color font-semibold  hover:border-text_color md:transition duration-300 text-xs md:text-base flex flex-col gap-1 items-center
+    ${hidden && "hidden md:flex"}
+    `}
+    >
+      <Icon size={25}/>
       <p>{title}</p>
     </div>
   );
